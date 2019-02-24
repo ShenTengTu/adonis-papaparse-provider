28a6e1e584f942b8f4fb087b780a6164fab237e1
Shen-Teng Tu <tusamten@gmail.com>
2019-02-24 17:23:50 +0800

    test(bodyparser): Update script structure
    Use `newRequestHandler(asyncCallback)` to create request handler for each test

--- test/functional/bodyParser.spec.js
+++ test/functional/bodyParser.spec.js
@@ -11,41 +11,51 @@ const PORT = 1234
 const URL = `http://localhost:${PORT}`
 const HELPER = new Helpers(join(__dirname, './'))
 
-async function requestHandler (req, res) {
-  const request = new Request(req)
+function newRequestHandler (asyncCallback) {
+  return async function requestHandler (req, res) {
+    const request = new Request(req)
 
-  const bodyParser = use('Adonis/Middleware/BodyParser')
-  const Papa = use('Papa')
+    const bodyParser = use('Adonis/Middleware/BodyParser')
 
-  await bodyParser.handle({ request }, async () => {
-    const importFile = request.file('importFile')
-    const tmpPath = HELPER.tmpPath('imports')
-    await importFile.move(tmpPath, { name: 'temp.csv', overwrite: true })
-
-    const stream = fs.createReadStream(join(tmpPath, 'temp.csv'), 'utf8')
-    const result = await new Promise ((resolve, reject) => {
-      Papa.parse(stream, {error:  reject, complete: resolve})
+    await bodyParser.handle({ request }, async () => {
+      await asyncCallback(request, res)
     })
-    
-    res.writeHead(200, { 'content-type': 'application/json' })
-    res.write(JSON.stringify(result))
-    res.end()
-  })
+  }
 }
 
 test.group('body Parser', (group) => {
   group.before(async () => {
-    await setup.up(PORT, requestHandler)
+
   })
 
   group.after(async () => {
-    await setup.down()
+
   })
 
   test('Upload CSV File to JSON', async (assert) => {
+    const  requestHandler = newRequestHandler(async (request, response) => {
+      const importFile = request.file('importFile')
+      const tmpPath = HELPER.tmpPath('imports')
+      await importFile.move(tmpPath, { name: 'temp.csv', overwrite: true })
+
+      const stream = fs.createReadStream(join(tmpPath, 'temp.csv'), 'utf8')
+      const result = await new Promise ((resolve, reject) => {
+        const Papa = use('Papa')
+        Papa.parse(stream, {error:  reject, complete: resolve})
+      })
+      
+      response.writeHead(200, { 'content-type': 'application/json' })
+      response.write(JSON.stringify(result))
+      response.end()
+    })
+    
+    await setup.up(PORT, requestHandler)
+  
     const {body} = await supertest(URL).post('/')
     .attach('importFile', join(__dirname, '../test.csv'))
 
     assert.sameDeepOrderedMembers(body.data, [['Subject', 'score'], ['Math', '82'], ['Physics', '76']], 'Default parse result')
+
+    await setup.down()
   })
 })
